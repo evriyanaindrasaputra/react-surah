48ffa2442fa3b81f9902edbf0ca66697bbd3b99b
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-06-23 21:06:44 +0700

    style: styling surah card

--- src/components/SurahCard/SurahCard.tsx
+++ src/components/SurahCard/SurahCard.tsx
@@ -10,17 +10,19 @@ type SurahCardProps = {
 
 const SurahCard : React.FC<SurahCardProps> = ({name, number, nameShort, translation, numberOfVerses}) => {
   return (
-    <div>
-      <div>
-        <span>{number}</span>
+    <div className='p-5 rounded-md hover:border-emerald-400 border-2 border-gray-500 flex gap-x-5 items-center bg-gray-500 justify-between cursor-pointer group'>
+      <div className='w-10 h-10 bg-gray-400 transition duration-100 ease-in rounded-sm rotate-45 flex justify-center items-center group-hover:bg-emerald-500 '>
+        <div className='-rotate-45 '>
+          <span className='text-gray-100 transition duration-100 ease-in  group-hover:text-gray-500 font-bold text-lg'>{number}</span>
+        </div>
+      </div>
+      <div className='flex-1 '>
+        <h3 className='text-gray-100 font-bold text-lg tracking-wide'>{name}</h3>
+        <span className='text-gray-300 group-hover:text-emerald-400 text-base'>{translation}</span>
       </div>
       <div>
-        <h3>{name}</h3>
-        <span>{translation}</span>
-      </div>
-      <div>
-        <p>{nameShort}</p>
-        <span>{numberOfVerses} ayat</span>
+        <p className=' text-gray-100 font-semibold text-base'>{nameShort}</p>
+        <span className='text-gray-300 group-hover:text-emerald-400 text-base'>{numberOfVerses} ayat</span>
       </div>
     </div>
   )
