9fa379bd5565dd8a69ac4074ab0ffde4f69d7c79
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-06-24 08:14:52 +0700

    style: :pencil2: typos usesurahstore

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,20 +5,37 @@ import {
 import { getAllSurah } from '~/lib/surah'
 import Hero from '~/components/Hero/Hero'
 import SurahCard from '~/components/SurahCard/SurahCard'
+import useSurahStore from '~/store/surah'
 
 const Home: React.FC = () => {
+  // store surah zustand 
+  const { setSurah } = useSurahStore()
+
   // Queries
-  const query = useQuery('ListSurah', getAllSurah)
+  const { isLoading, isError, data, error } = useQuery('ListSurah', getAllSurah, {
+    onSuccess:
+      data => {
+        setSurah(data)
+      }
+  })
+
   return (
     <>
       <Hero />
-      <SurahCard 
-        name={'Al Fatihah'} 
-        nameShort={'الفاتحة'} 
-        translation={'Pembukaan'} 
-        number={1} 
-        numberOfVerses={7} 
-      />
+      <div className='grid grid-cols-3 gap-4'>
+        {
+          !isLoading  &&
+          data?.map(item =>
+            <SurahCard
+              name={item.name.transliteration.id}
+              nameShort={item.name.short}
+              translation={item.name.translation.id}
+              number={item.number}
+              numberOfVerses={item.numberOfVerses}
+            />
+          )
+        }
+      </div>
     </>
   )
 }
