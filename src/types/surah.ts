cdce9b72b712b20565ee9b8aa08ce0f9b707f9f3
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-06-27 11:00:55 +0700

    feat: add type verse

--- src/types/surah.ts
+++ src/types/surah.ts
@@ -1,20 +1,55 @@
+export interface Verse {
+  audio: {
+    primary: string,
+    secondary: Array<String>
+  }
+  meta: {
+    juz: number,
+    page: number,
+    ruku: number,
+  }
+  number: {
+    inQuran: number,
+    inSurah: number,
+  }
+  tafsir: {
+    id: {
+      long: string,
+      short: string,
+    }
+  }
+  text: {
+    arab: string,
+    transliteration: {
+      en: string,
+    }
+  }
+  translation: {
+    en: string,
+    id: string,
+  }
+}
+
+
 export interface Surah {
-  name : {
-    long : string,
-    short : string,
-    translation : {
-      en : string,
-      id : string,
+  name: {
+    long: string,
+    short: string,
+    translation: {
+      en: string,
+      id: string,
     },
-    transliteration : {
-      en : string,
-      id : string,
+    transliteration: {
+      en: string,
+      id: string,
     }
   },
-  number : number,
-  numberOfVerses : number,
-  sequence : number,
-  tafsir : {
+  number: number,
+  numberOfVerses: number,
+  sequence: number,
+  tafsir: {
     id: string
   }
 }
+
+
